fdfe98cb0cc1278dbb89bab4f0804a7562915323
GallonChoi <404842441@qq.com>
2015-09-04 16:08:20 +0000

    fix typos

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include "logger.hpp"
 
+using Logger::LoggingHandler;
+using Logger::logInfo;
+using Logger::logDebug;
+using Logger::logWarning;
+using Logger::logError;
+
+
+void threadTest(int idx) {
+    logInfo("Thread: " + std::to_string(idx));
+}
+
 
 int main(void) {
-    Logger::LoggingHandler.init();
-    Logger::logInfo("Init");
+    LoggingHandler.init();
+    std::thread threads[30];
+    for (int idx = 0; idx < 30; ++ idx)
+        threads[idx] = std::thread(threadTest, idx);
+
+    for(auto& thread : threads)
+        thread.join();
+
     return 0;
 }
